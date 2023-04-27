eb568ba293dc490a1ee275b6c556a26b4af98d95
Rohith <varma36a@gmail.com>
2023-04-27 09:08:13 +0530

    Singleton patterns

---
+++ CreationalDesign/Singleton/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Client
+    {
+        public void Add()
+        {
+            try
+            {
+                // TO DO:
+
+            }
+            catch (Exception ex)
+            {
+                // var obj = new ExceptionHandler();
+                ExceptionHandler.GetInstance.WriteLog(ex.Message);
+            }
+        }
+    }
+}
---
+++ CreationalDesign/Singleton/ExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Singleton
+{
+    public class ExceptionHandler
+    {
+        private static ExceptionHandler _eh;
+        private static StreamWriter _sw;
+        private static object _obj = new object();
+
+
+        public ExceptionHandler()
+        {
+            _sw = new StreamWriter(@"C:\Error.txt");
+        }
+
+
+        public static ExceptionHandler GetInstance
+        {
+            get{
+
+                lock (_obj)
+                {
+                    if (_eh == null)
+                        _eh = new ExceptionHandler();
+                    return _eh;
+                }
+
+            }
+        }
+
+        public void WriteLog(string msg)
+        {
+            _sw.WriteLine(msg);
+            _sw.Flush();
+        }
+
+
+    }
+}
